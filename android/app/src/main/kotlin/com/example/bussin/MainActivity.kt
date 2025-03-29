6523508768765c7fa671fceba81e7e065e07110e
jrolana <olanajhoanna@gmail.com>
2025-03-30 00:42:39 +0800

    [Magbanua][Olana] Setup

---
+++ android/app/src/main/kotlin/com/example/bussin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bussin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'slot_machine/slot_machine_roller.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var targets = List<int?>.filled(3, 1);
+
+  Future<void> _incrementCounter() async {
+    setState(() {
+      targets = List<int?>.filled(3, null);
+    });
+    await Future.delayed(const Duration(seconds: 5));
+    setState(() {
+      targets = List<int?>.filled(3, 3);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(child: SlotMachine(targets: targets)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Rolling',
+        child: const Icon(Icons.rocket_launch_outlined),
+      ),
+    );
+  }
+}
